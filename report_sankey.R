ece051f4abbe555329c7263e7453cb725de8cee9
Luca Lamoni <lucalamoni@gmail.com>
2025-11-20 09:48:28 +0100

    Increase font size in Sankey diagrams for improved readability

--- report_sankey.R
+++ report_sankey.R
@@ -101,7 +101,7 @@ p <- sankeyNetwork(
   Target = "IDtarget",
   Value = "value", 
   NodeID = "name",
-  fontSize = 24, 
+  fontSize = 26, 
   units = "Impact Risk",
   nodeWidth = 28,
   colourScale = my_color,
@@ -138,7 +138,7 @@ p2 <- sankeyNetwork(
   units = "Impact Risk",
   LinkGroup = "Confidence",
   NodeGroup = "node_group",
-  fontSize = 24, 
+  fontSize = 26, 
   nodeWidth = 28,
   colourScale = col_scale
   # iterations = 0
